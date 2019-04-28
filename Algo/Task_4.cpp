90574042ef99e9e9f724b8793479dd2a9d8ab105
Дмитрий <@DESKTOP-N4D3ITB>
2019-04-28 22:27:56 +0400

    task 4

--- Algo/Task_4.cpp
+++ Algo/Task_4.cpp
@@ -12,6 +12,26 @@ struct Elements {
 	double *arr = new double[8];
 };
 
+struct LoopElements {
+	unsigned int headIndex = 0;
+	unsigned int tailIndex = 0;
+	unsigned int length = 0;
+	unsigned int size = 8;
+	double *arr = new double[8];
+};
+
+struct LinkedElement {
+	double data;
+	LinkedElement *nextElement;
+	LinkedElement *previousElement;
+};
+
+struct LinkedElements {
+	LinkedElement *head = NULL;
+	LinkedElement *tail;
+	unsigned int length = 0;
+};
+
 void increaseArray(Elements *element) {
 	double *temp = new double[element->size * 2];
 	for (int i = 0; i < element->size; i++) {
@@ -19,6 +39,32 @@ void increaseArray(Elements *element) {
 	}
 	element->size *= 2;
 	element->arr = temp;
+}
+
+void pushLinkedStack(double data, LinkedElements *elements) {
+	LinkedElement *element = new LinkedElement;
+	element->data = data;
+	if (elements->length == 0) {
+		elements->head = element;
+	}
+	else {
+		element->previousElement = elements->head;
+		elements->head->nextElement = element;
+		elements->head = element;
+	}
+	elements->length++;
+}
+
+LinkedElement popLinkedStack(LinkedElements *elements) {
+	if (elements->length == 0) {
+		cout << "No elements in stack" << endl;
+	}
+	else {
+		LinkedElement *result = elements->head;
+		elements->head = elements->head->previousElement;
+		elements->length--;
+		return *result;
+	}
 }
 
 void pushArrayQueue(double data, Elements *element) {
@@ -42,28 +88,35 @@ double popArrayQueue(Elements *element) {
 	}
 }
 
-void pushLoopQueue(double data, Elements *element) {
-	if (element->length != element->size) {
-		element->arr[element->length++] = data;
+void pushLoopQueue(double data, LoopElements *elements) {
+	if (elements->length == elements->size) {
+		cout << "Enter element is impossible" << endl;
 	}
 	else {
-		for (int i = 0; i < element->size - 1; i++) {
-			element->arr[i] = element->arr[i + 1];
+		elements->arr[elements->tailIndex] = data;
+		elements->length++;
+		if (elements->tailIndex == elements->size - 1) {
+			elements->tailIndex = 0;
 		}
-		element->arr[element->size - 1] = data;
+		else {
+			elements->tailIndex++;
+		}
 	}
 }
 
-double popLoopQueue(Elements *element) {
-	if (element->length == 0) {
-		cout << "No element in this queue" << endl;
+double popLoopQueue(LoopElements *elements) {
+	if (elements->length == 0) {
+		cout << "No elements in queue" << endl;
 	}
 	else {
-		double result = element->arr[0];
-		for (int i = 0; i < element->length - 1; i++) {
-			element->arr[i] = element->arr[i + 1];
+		double result = elements->arr[elements->headIndex];
+		elements->length--;
+		if (elements->headIndex == elements->size - 1) {
+			elements->headIndex = 0;
 		}
-		element->length--;
+		else {
+			elements->headIndex++;
+		}
 		return result;
 	}
 }
@@ -83,24 +136,6 @@ double popArrayStack(Elements *element) {
 		double result = element->arr[element->length-- - 1];
 		return result;
 	}
-}
-
-void pushLoopStack(double data, Elements *element) {
-	if (element->length != element->size) {
-		element->arr[element->length++] = data;
-	}
-	else {
-		for (int i = 0; i < element->size - 1; i++) {
-			element->arr[i] = element->arr[i + 1];
-		}
-		element->arr[element->size - 1] = data;
-	}
-	
-}
-
-double popLoopStack(Elements *element) {
-	double result = element->arr[element->length-- - 1];
-	return result;
 }
 
 double popPriorityQueue(Elements *element) {
@@ -131,7 +166,7 @@ int main() {
 	double data;
 	int iterator;
 
-	/*cout << "Test array queue" << endl;
+	cout << "Test array queue" << endl;
 	cout << "Enter count element in queue" << endl;
 	cin >> n;
 	Elements queue;
@@ -141,14 +176,14 @@ int main() {
 		pushArrayQueue(data, &queue);
 	}
 
-	cout << "Print all elements in queue" << endl;
+	cout << "Print all elements from queue" << endl;
 	iterator = 0;
-	while (queue.length > 0) {
+	while (queue.length != 0) {
 		cout << iterator++ << " element: " << popArrayQueue(&queue) << endl;
-	}*/
+	}
 
-	/*Elements loopQueue;
 	cout << "Test loop queue" << endl;
+	LoopElements loopQueue;
 	cout << "Enter 10 elements in queue" << endl;
 	for (int i = 0; i < 10; i++) {
 		cout << "Enter " << i << " element in loop queue" << endl;
@@ -156,13 +191,25 @@ int main() {
 		pushLoopQueue(data, &loopQueue);
 	}
 
-	cout << "Print all elements in loop queue" << endl;
-	iterator = 0;
-	while (loopQueue.length > 0) {
-		cout << iterator++ << " element: " << popLoopQueue(&loopQueue) << endl;
-	}*/
+	cout << "Pop 3 elements from loop queue" << endl;
+	for (int i = 0; i < 3; i++) {
+		cout << i << " element: " << popLoopQueue(&loopQueue) << endl;
+	}
 
-	/*cout << "Test array stack" << endl;
+	cout << "Enter 3 elements in loop queue" << endl;
+
+	for (int i = 0; i < 3; i++) {
+		cin >> data;
+		pushLoopQueue(data, &loopQueue);
+	}
+
+	cout << "Print all elements from loop queue" << endl;
+	iterator = 0;
+	while (loopQueue.length != 0) {
+		cout << iterator++ << " element: " << popLoopQueue(&loopQueue) << endl;
+	}
+
+	cout << "Test array stack" << endl;
 	cout << "Enter count elements in stack" << endl;
 	cin >> n;
 	Elements arrayStack;
@@ -172,26 +219,27 @@ int main() {
 		pushArrayStack(data, &arrayStack);
 	}
 
-	cout << "Print all element in stack" << endl;
+	cout << "Print all element from stack" << endl;
 	iterator = 0;
 	while (arrayStack.length != 0) {
 		cout << iterator++ << " element: " << popArrayStack(&arrayStack) << endl;
-	}*/
-
-	/*cout << "Test loop stack" << endl;
-	Elements loopStack;
-	cout << "Enter 10 elements in stack" << endl;
-	for (int i = 0; i < 10; i++) {
-		cout << "Enter " << i << " element in loop stack" << endl;
-		cin >> data;
-		pushLoopStack(data, &loopStack);
 	}
 
-	cout << "Print all elements in loop stack" << endl;
+	cout << "Test linked stack" << endl;
+	cout << "Enter count elements in stack" << endl;
+	cin >> n;
+	LinkedElements linkedStack;
+	for (int i = 0; i < n; i++) {
+		cout << "Enter " << i << " element in stack" << endl;
+		cin >> data;
+		pushLinkedStack(data, &linkedStack);
+	}
+
+	cout << "Print all elements from linked stack" << endl;
 	iterator = 0;
-	while (loopStack.length != 0) {
-		cout << iterator++ << " element in loop stack: " << popLoopStack(&loopStack) << endl;
-	}*/
+	while (linkedStack.length != 0) {
+		cout << iterator++ << " element in linked stack: " << popLinkedStack(&linkedStack).data << endl;
+	}
 
 	cout << "Test priority queue" << endl;
 	cout << "Enter count elements in priority queue" << endl;
@@ -203,12 +251,10 @@ int main() {
 		pushArrayQueue(data, &priorityQueue);
 	}
 
-	cout << "Print all elements in priority queue" << endl;
+	cout << "Print all elements from priority queue" << endl;
 	iterator = 0;
 	while (priorityQueue.length != 0) {
 		cout << iterator++ << " element in priority queue: " << popPriorityQueue(&priorityQueue) << endl;
 	}
-
-
 	return 0;
 }
